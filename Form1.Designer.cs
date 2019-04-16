6ee8ad5fe048b9281b1973b449e8398b9170ee38
Bryan Santos <49533832+BryanSantoos@users.noreply.github.com>
2019-04-16 19:25:43 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class frmAnimais
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboAnimais = new System.Windows.Forms.ComboBox();
+            this.btnPatas = new System.Windows.Forms.Button();
+            this.pctAnimais = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAnimais)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboAnimais
+            // 
+            this.cboAnimais.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboAnimais.FormattingEnabled = true;
+            this.cboAnimais.Items.AddRange(new object[] {
+            "Cachorro ",
+            "Cavalo ",
+            "Centopeia",
+            "Cobra",
+            "Gato ",
+            "Pássaro"});
+            this.cboAnimais.Location = new System.Drawing.Point(41, 21);
+            this.cboAnimais.Name = "cboAnimais";
+            this.cboAnimais.Size = new System.Drawing.Size(153, 21);
+            this.cboAnimais.Sorted = true;
+            this.cboAnimais.TabIndex = 0;
+            this.cboAnimais.SelectedIndexChanged += new System.EventHandler(this.cboAnimais_SelectedIndexChanged);
+            // 
+            // btnPatas
+            // 
+            this.btnPatas.Location = new System.Drawing.Point(60, 48);
+            this.btnPatas.Name = "btnPatas";
+            this.btnPatas.Size = new System.Drawing.Size(110, 23);
+            this.btnPatas.TabIndex = 1;
+            this.btnPatas.Text = "Mostra Patas";
+            this.btnPatas.UseVisualStyleBackColor = true;
+            this.btnPatas.Click += new System.EventHandler(this.btnPatas_Click);
+            // 
+            // pctAnimais
+            // 
+            this.pctAnimais.Location = new System.Drawing.Point(16, 77);
+            this.pctAnimais.Name = "pctAnimais";
+            this.pctAnimais.Size = new System.Drawing.Size(198, 146);
+            this.pctAnimais.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctAnimais.TabIndex = 3;
+            this.pctAnimais.TabStop = false;
+            this.pctAnimais.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // frmAnimais
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 276);
+            this.Controls.Add(this.pctAnimais);
+            this.Controls.Add(this.btnPatas);
+            this.Controls.Add(this.cboAnimais);
+            this.Name = "frmAnimais";
+            this.Text = "Animais";
+            this.Load += new System.EventHandler(this.cboAnimais_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctAnimais)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboAnimais;
+        private System.Windows.Forms.Button btnPatas;
+        private System.Windows.Forms.PictureBox pctAnimais;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class frmAnimais : Form
+    {
+        public frmAnimais()
+        {
+            InitializeComponent();
+        }
+
+        private void cboAnimais_Load(object sender, EventArgs e)
+        {
+            //limpa os itens adicionados no ComboBox
+            cboAnimais.Items.Clear();
+
+            //adiciona os Itens no ComboBox 
+            //por meio do méto Add( )
+
+            cboAnimais.Items.Add("Cachorro");
+            cboAnimais.Items.Add("Cavalo");
+            cboAnimais.Items.Add("Gato");
+            cboAnimais.Items.Add("Centopeia");
+            cboAnimais.Items.Add("Cobra");
+            cboAnimais.Items.Add("Pássaro");
+        }
+
+        private void cboAnimais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnPatas_Click(object sender, EventArgs e)
+        {
+            //declara variável
+            String strAnimal;
+
+            //variável recebe o texto do item do ComboBox Selecionado
+            strAnimal = cboAnimais.Text;
+
+            //instrução de seleção
+            switch (strAnimal)
+            {
+                case "Cachorro":
+
+                    //carrega o arquivo que está na pasta Debug
+                    pctAnimais.Load("cachorro.png");
+                    break;
+                case "Cavalo":
+                    pctAnimais.Load("cavalo.png");
+                    MessageBox.Show("Este animal tem 4 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "Centopeia":
+                    pctAnimais.Load("centopeia.png");
+                    MessageBox.Show("Este animal tem 100 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "Cobra":
+                    pctAnimais.Load("cobra.png");
+                    MessageBox.Show("Este animal não tem patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "Gato":
+                    pctAnimais.Load("gato.png");
+                    MessageBox.Show("Este animal tem 4 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "Pássaro":
+                    pctAnimais.Load("passaro.png");
+                    MessageBox.Show("Este animal tem 2 patas", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                default:
+                    //limpa o PictureBox
+                    pctAnimais.ResetText();
+                    MessageBox.Show("Selecione um Animal", "Animais", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
